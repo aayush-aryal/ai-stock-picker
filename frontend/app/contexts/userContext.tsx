86dadd8f5574c237d1acacc766e8d03672060622
aayush-aryal <rinnegan2024@gmail.com>
2025-11-18 18:36:08 -0600

    added user context to prevent fetching of user all over

---
+++ frontend/app/contexts/userContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { UserDTO } from "../definitions";
+
+const UserContext = createContext<{
+  user: UserDTO | null;
+  setUser: (u: UserDTO) => void;
+}>({ user: null, setUser: () => {} });
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<UserDTO | null>(null);
+  useEffect(() => {
+    async function fetchUser() {
+      const resp = await fetch("http://localhost:8000/auth/users/me", {
+        credentials: "include",
+      });
+      console.log("resp", resp);
+      if (!resp.ok) return;
+      const user: UserDTO = await resp.json();
+      setUser(user);
+    }
+
+    fetchUser();
+  }, []);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
